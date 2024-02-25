97ca1c8e1db9115e290448306b0b193db7281b1f
OuafidiOussama <oussamaouafidi0@gmail.com>
2024-02-25 22:19:44 +0100

    KIC-33 The user now can search for a player by their full name and view their details

--- src/screens/Players.jsx
+++ src/screens/Players.jsx
@@ -26,8 +26,13 @@ export default function Players() {
   } else if (status === "success") {
     return (
       <ScrollView>
-        <View>
-          
+        <View style={styles.container}>
+          <TextInput
+            value={search}
+            onChangeText={setSearch}
+            placeholder="Search For Player.."
+            style={styles.input}
+          />
           {players?.map((player) => (
             <PlayerCard key={player.id} player={player} />
           ))}
@@ -43,4 +48,13 @@ export default function Players() {
   }
 }
 
-
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    marginHorizontal: 40,
+    marginVertical: 10,
+    borderRadius: 10,
+    borderColor: 'gray'
+  },
+});
